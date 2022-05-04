a2c56f38a8bc7526f389a0ee87f4d19977b2dd52
bokessepv <bongondajaphet@gmail.com>
2022-05-04 08:21:13 +0200

    Modification: base.html.twig -> changement du bootstrap
    SortieController.php -> Ajout de la fonction modifier + creation de la page modifier sortie et creation sortie.
    
    footer.html.twig -> Ajout du footer, a completer.
    
    Participant + Campus + Sortie Entite -> suppression des mapping entre campus + Participant , Participant et sortie (organisateur) et Sortie Campus.
    
    Ajout de lieu type -> double formulaire dans creer sortie, l'un a un formulaire de sortie et l'autre un formulaire de lieu
    
    Header -> Changement final du header, complet
    
    Quelques ajout mineur, nouvelles image du site + icon

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,11 +2,8 @@
 
 namespace App\Form;
 
-use App\Entity\Campus;
-use App\Entity\Etat;
 use App\Entity\Lieu;
 use App\Entity\Sortie;
-use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
